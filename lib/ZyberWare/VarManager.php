ec2194bb74379f849aada6de2f5346a09ddb5581
zyberspace <zyberspace@zyberware.org>
2014-04-13 04:30:11 +0200

    Updated Copyright

--- lib/ZyberWare/VarManager.php
+++ lib/ZyberWare/VarManager.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2013 Eric Enold <zyberspace@zyberware.org>
+ * Copyright 2014 Eric Enold <zyberspace@zyberware.org>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
